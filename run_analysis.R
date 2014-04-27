de503755a773410130074314eebf23f6b27419c0
Muds <muds@users.noreply.github.com>
2014-04-26 22:32:15 -0600

    Switching approach for generating dataframe from zip, to use a less general structure.

--- run_analysis.R
+++ run_analysis.R
@@ -1,49 +1,92 @@
-# The Samsung data are stored in the following ZIP archive, which is
-# assumed to be in the current working directory.
-zipfile <- 'getdata-projectfiles-UCI HAR Dataset.zip'
-
-# Retrieve a list of all filenames stored in the archive
-fnames <- unzip(zipfile, list = T)
-
-# Subset those files whose names contain the string 'X_test.txt'
-# or 'y_test.txt'
-testfiles <- sort(fnames$Name[grepl('/(X|y)_test.txt', fnames$Name)])
-# And those containing 'X_train.txt' or 'y_train.txt'
-trainfiles <- sort(fnames$Name[grepl('/(X|y)_train.txt', fnames$Name)])
-# We now have two lists of two files each, which need to be combined
-# pairwise (i.e. first with first, and second with second).
-
-# Define a function that will combine two of the files into one
-combineFiles <- function (ftest, ftrain) {
-  # We want to turn 'dir/file_test.txt' into 'file.txt', which will
-  # be the output filename for the combination of the two input files
-  start <- nchar('UCI HAR Dataset/test/') + 1
-  # Remove the initial path info
-  fname <- substring(ftest, start)
-  # Remove the end of the filename
-  end <- nchar(fname) - nchar('_test.txt')
-  fname <- substring(fname, 1, end)
-  # We now have 'file'; append '.txt' with no separation
-  fname <- paste(fname, '.txt', sep='')
-  # fname is now 'file.txt' as desired
+genDataframeFromZip <- function (
+  zipfile = 'getdata-projectfiles-UCI HAR Dataset.zip'
+) {
+  # Within the zip file, the following files contain data we want.
+  # Store the names in a dataframe such that datafiles['var', 'type'],
+  # with 'type' equal to either 'test' or 'train', will return the
+  # path and filename to 
+  datafiles <- data.frame(
+    test = c(
+      'UCI HAR Dataset/test/X_test.txt',
+      'UCI HAR Dataset/test/y_test.txt',
+      'UCI HAR Dataset/test/subject_test.txt'
+    ),
+    train = c(
+      'UCI HAR Dataset/train/X_train.txt',
+      'UCI HAR Dataset/train/y_train.txt',
+      'UCI HAR Dataset/train/subject_train.txt'
+    ),
+    
+    # End of data, start of data.frame parameters
+    row.names = c(
+      'X', # Actual values measured and recorded during each test
+      'y', # Task ID, specifying which task was performed during the test
+      'subject' # Person ID, specifying the subject who performed the test
+    ),
+    stringsAsFactors = F
+  )
   
-  # Since combining the files is somewhat time consuming, we
-  # only want to do it if it hasn't already been done.
-  if (!file.exists(fname)) {
-    # Next step into it Combine the two files into one
-    testData <- read.table(unz(zipfile, ftest), headers = F)
-    trainData <- read.table(unz(zipfile, ftrain), headers = F)
+  # Function to return the combined data set in two files (test and train)
+  combineFiles <- function (ftest, ftrain) {
+    testData <- read.table(unz(zipfile, ftest))
+    trainData <- read.table(unz(zipfile, ftrain))
   
-    data <- rbind(testData, trainData)
-  
-    write.table(data, file = fname)
+    rbind(testData, trainData)
   }
   
-  # Return the processed filename as the function output
-  fname
+  # Create an empty object that will have data added to it as the files
+  # are read and combined
+  dataset <- NULL
+  
+  #### Step 1: Combine the 'test' and 'train' datasets
+  for (i in seq(nrow(datafiles))) {
+    #dataset[rownames(datafiles)[i]]
+    newdata <- combineFiles(
+      datafiles[i, 'test'],
+      datafiles[i, 'train']
+    )
+    if (is.null(dataset)) {
+      dataset <- newdata
+    } else {
+      dataset <- cbind(dataset, newdata)
+    }
+  }
+
+  dataset
 }
 
-#### Step 1: Combine the 'test' and 'train' datasets
-for (i in seq_along(testfiles)) {
-  combineFiles(testfiles[i], trainfiles[i])
+genDataframeFromZip <- function () {
+  zipfile = 'getdata-projectfiles-UCI HAR Dataset.zip'
+  
+  # Function to return the combined data set in two files (test and train)
+  combineFiles <- function (ftest, ftrain) {
+    testData <- read.table(unz(zipfile, ftest))
+    trainData <- read.table(unz(zipfile, ftrain))
+    
+    rbind(testData, trainData)
+  }
+  
+  X <- combineFiles(
+    'UCI HAR Dataset/test/X_test.txt',
+    'UCI HAR Dataset/train/X_train.txt'
+  )
+  y <- combineFiles(
+    'UCI HAR Dataset/test/y_test.txt',
+    'UCI HAR Dataset/train/y_train.txt'
+  )
+  subject <- combineFiles(
+    'UCI HAR Dataset/test/subject_test.txt',
+    'UCI HAR Dataset/train/subject_train.txt'
+  )
+  
+  features <- read.table(
+    unz(zipfile, 'UCI HAR Dataset/features.txt'),
+    stringsAsFactors = F
+  )
+  colnames(features) <- c('feat.id', 'description')
+  
+  data <- cbind(y, subject, X)
+  colnames(data) <- c('task.id', 'subject.id', features$description)
+  
+  data
 }
