727039991ba77f8b707ebaae619e93a4a948bcf0
Muds <muds@users.noreply.github.com>
2014-04-26 23:38:24 -0600

    Tidy data set creation essentially complete.

--- run_analysis.R
+++ run_analysis.R
@@ -1,78 +1,38 @@
-genDataframeFromZip <- function (
-  zipfile = 'getdata-projectfiles-UCI HAR Dataset.zip'
-) {
-  # Within the zip file, the following files contain data we want.
-  # Store the names in a dataframe such that datafiles['var', 'type'],
-  # with 'type' equal to either 'test' or 'train', will return the
-  # path and filename to 
-  datafiles <- data.frame(
-    test = c(
-      'UCI HAR Dataset/test/X_test.txt',
-      'UCI HAR Dataset/test/y_test.txt',
-      'UCI HAR Dataset/test/subject_test.txt'
-    ),
-    train = c(
-      'UCI HAR Dataset/train/X_train.txt',
-      'UCI HAR Dataset/train/y_train.txt',
-      'UCI HAR Dataset/train/subject_train.txt'
-    ),
-    
-    # End of data, start of data.frame parameters
-    row.names = c(
-      'X', # Actual values measured and recorded during each test
-      'y', # Task ID, specifying which task was performed during the test
-      'subject' # Person ID, specifying the subject who performed the test
-    ),
-    stringsAsFactors = F
-  )
-  
-  # Function to return the combined data set in two files (test and train)
-  combineFiles <- function (ftest, ftrain) {
-    testData <- read.table(unz(zipfile, ftest))
-    trainData <- read.table(unz(zipfile, ftrain))
-  
-    rbind(testData, trainData)
-  }
-  
-  # Create an empty object that will have data added to it as the files
-  # are read and combined
-  dataset <- NULL
-  
-  #### Step 1: Combine the 'test' and 'train' datasets
-  for (i in seq(nrow(datafiles))) {
-    #dataset[rownames(datafiles)[i]]
-    newdata <- combineFiles(
-      datafiles[i, 'test'],
-      datafiles[i, 'train']
-    )
-    if (is.null(dataset)) {
-      dataset <- newdata
-    } else {
-      dataset <- cbind(dataset, newdata)
-    }
-  }
-
-  dataset
-}
-
 genDataframeFromZip <- function () {
   zipfile = 'getdata-projectfiles-UCI HAR Dataset.zip'
   
   # Function to return the combined data set in two files (test and train)
-  combineFiles <- function (ftest, ftrain) {
+  combineFiles <- function (ftest, ftrain, cnames = NULL) {
     testData <- read.table(unz(zipfile, ftest))
     trainData <- read.table(unz(zipfile, ftrain))
     
-    rbind(testData, trainData)
+    fullSet <- rbind(testData, trainData)
+    if (!is.null(cnames)) {
+      colnames(fullSet) <- cnames
+    }
+    
+    fullSet
   }
+
+  # Retrieve the list of Activity IDs in the order in which they were recorded
+  activity.performed <- combineFiles(
+    'UCI HAR Dataset/test/y_test.txt',
+    'UCI HAR Dataset/train/y_train.txt',
+    c('id')
+  )
+  # Definition of the activity IDs and corresponding names
+  activity.defn <- read.table(
+    unz(zipfile, 'UCI HAR Dataset/activity_labels.txt')
+  )
+  colnames(activity.defn) <- c('id', 'activity')
+  # Create a vector of the names of the activities by row
+  activity <- activity.defn$activity[
+    match(activity.performed$id, activity.defn$id)
+  ]
   
-  X <- combineFiles(
+  results <- combineFiles(
     'UCI HAR Dataset/test/X_test.txt',
     'UCI HAR Dataset/train/X_train.txt'
-  )
-  y <- combineFiles(
-    'UCI HAR Dataset/test/y_test.txt',
-    'UCI HAR Dataset/train/y_train.txt'
   )
   subject <- combineFiles(
     'UCI HAR Dataset/test/subject_test.txt',
@@ -85,8 +45,19 @@ genDataframeFromZip <- function () {
   )
   colnames(features) <- c('feat.id', 'description')
   
-  data <- cbind(y, subject, X)
-  colnames(data) <- c('task.id', 'subject.id', features$description)
+  # Create a logical vector that is TRUE only for features whose description
+  # contains either 'mean()' or 'std()'
+  wantedFeats <- grepl('-(mean|std)\\(\\)', features$description)
+  
+  # Bind together the columns of the Task ID, Subject ID, and wanted features
+  data <- cbind(activity, subject, results[,wantedFeats])
+  # Give these columns appropriate names
+  colnames(data) <- c(
+    'activity',
+    'subject',
+    features[wantedFeats,'description']
+  )
   
   data
 }
+
